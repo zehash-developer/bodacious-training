a11c6aaddd994b1ce5398562094f6b04c1008682
frownakk <farhana.rownak@gmail.com>
2021-09-30 11:45:06 +1000

    Solved the magic square problem.

---
+++ farhana/magicSquare.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'formingMagicSquare' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY s as parameter.
+ */
+
+function formingMagicSquare(s) {
+    // Write your code here
+    let flatArray = s.flat(3);
+    let cost = 0;
+    let min = 45;
+    
+    const magicSquares = [
+        [8, 1, 6, 3, 5, 7, 4, 9, 2],
+        [6, 1, 8 ,7, 5, 3, 2, 9, 4],
+        [4, 9, 2, 3, 5, 7, 8, 1, 6],
+        [2, 9, 4, 7, 5, 3, 6, 1, 8],
+        [8, 3, 4, 1, 5, 9, 6, 7, 2],
+        [4, 3, 8, 9, 5, 1, 2, 7, 6],
+        [6, 7, 2, 1, 5, 9, 8, 3, 4],
+        [2, 7, 6, 9, 5, 1, 4, 3, 8],
+    ];
+
+for (let i = 0; i < magicSquares.length; i++) {
+    cost = 0;
+    for (let j = 0; j < flatArray.length; j++) {
+        cost += Math.abs(flatArray[j] - magicSquares[i][j]);
+    }
+    if (cost < min) {
+        min = cost;
+    }
+}
+return min;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let s = Array(3);
+
+    for (let i = 0; i < 3; i++) {
+        s[i] = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+    }
+
+    const result = formingMagicSquare(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+
